49f902f5ef9ae0fc050534f9ef8bd1333d54f512
Sebastien Ong Tone <s.ongtone@hotmail.com>
2017-10-13 02:04:51 -0400

    added the camera controls x and y axis

--- COMP371/COMP371/Main.cpp
+++ COMP371/COMP371/Main.cpp
@@ -25,13 +25,15 @@ glm::vec3 camera_position;
 glm::vec3 triangle_scale;
 glm::mat4 projection_matrix;
 
-float x;
-float y;
+float cameraX = 0.0f;
+float cameraY = 0.0f;
+float cameraZ = 2.0f;
+float panning = 0.0f;
 
 // Constant vectors
 const glm::vec3 center(0.0f, 0.0f, 0.0f);
 const glm::vec3 up(0.0f, 1.0f, 0.0f);
-const glm::vec3 eye(0.0f, 0.0f, 3.0f);
+const glm::vec3 eye(0.0f, 0.0f, 10.0f);
 
 //functions used
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
@@ -164,27 +166,33 @@ int main()
 
 	glUseProgram(shaderProgram);
 
-	//Show the image that will be used
-	CImg<float> image("depth.bmp");
-	CImgDisplay main_disp(image, "The Depth Picture");
+	//Show the picture that will be used
+	CImg<float> picture("depth.bmp");
+	CImgDisplay main_disp(picture, "The Depth Picture");
 
-	cout << image.size() << endl;
+	cout << picture.size() << endl;
 	//Pixel Data
-	
+	vector <glm::vec3> pictureData;
 
-	//iteration through the image pixels
-	for (int i = 0; i <= image.size(); i++){
-
+	//iteration through the picture pixels
+	int x = (0 - picture.width() / 2);
+	int z = (0 - picture.height() / 2);
+	for (CImg<float>::iterator i = picture.begin(); i < picture.end(); i++){
+		pictureData.emplace_back(glm::vec3(x++, *i, z));
+		if (x == picture.width()) {
+			x = (0 - picture.width() / 2);
+			z += 1;
+		}
 }
 
-	GLuint VAO, VBO;
-	//glGenVertexArrays(1, &VAO);
-	//glGenBuffers(1, &VBO);
+	//GLuint VAO_pic, VBO_pic;
+	//glGenVertexArrays(1, &VAO_pic);
+	//glGenBuffers(1, &VBO_pic);
 
 	//// Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s).
-	//glBindVertexArray(VAO);
+	//glBindVertexArray(VAO_pic);
 
-	//glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	//glBindBuffer(GL_ARRAY_BUFFER, VBO_pic);
 	//glBufferData(GL_ARRAY_BUFFER, picture.size() * sizeof(glm::vec3), &picture.front(), GL_STATIC_DRAW);
 	//glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
 	//glEnableVertexAttribArray(0);
@@ -193,7 +201,27 @@ int main()
 
 	//glBindVertexArray(0); // Unbind VAO (it's always a good thing to unbind any buffer/array to prevent strange bugs), remember: do NOT unbind the EBO, keep it bound to this VAO
 
+	float vertices[] = {//triangle coordinates
+		-0.5f, -0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f,
+		0.0f, 0.5f, 0.0f
+	};
 
+	glm::vec3 triposition[]{
+		glm::vec3(2.0f, 5.0f, -15.0f)
+	};
+
+	unsigned int VBO;
+	unsigned int VAO;
+	glGenBuffers(1, &VBO);
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
 
 	GLuint projectionLoc = glGetUniformLocation(shaderProgram, "projection_matrix");
 	GLuint viewMatrixLoc = glGetUniformLocation(shaderProgram, "view_matrix");
@@ -210,11 +238,15 @@ int main()
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		glm::vec3 center(cameraX, cameraY, 0.0f);
+		glm::vec3 up(0.0f, 1.0f, 0.0f);
+		glm::vec3 eye(cameraX, cameraY, cameraZ);
+
 		glm::mat4 view_matrix;
 		view_matrix = glm::lookAt(eye, center, up);
 
 		glm::mat4 model_matrix;
-		model_matrix = glm::scale(model_matrix, triangle_scale);
+		
 
 		glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(model_matrix));
 		glUniformMatrix4fv(viewMatrixLoc, 1, GL_FALSE, glm::value_ptr(view_matrix));
@@ -224,7 +256,12 @@ int main()
 		glDrawArrays(GL_TRIANGLES, 0, picture.size());
 		glBindVertexArray(0);*/
 
+
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindVertexArray(0);
 		// Swap the screen buffers
+		
 		glfwSwapBuffers(window);
 	}
 
@@ -236,6 +273,19 @@ int main()
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
 {
 	std::cout << key << std::endl;
+
+	if (key == GLFW_KEY_UP && action == GLFW_PRESS) {
+		cameraY = cameraY + 0.2f;
+	}
+	if (key == GLFW_KEY_DOWN && action == GLFW_PRESS) {
+		cameraY = cameraY - 0.2f;
+	}
+	if (key == GLFW_KEY_RIGHT && action == GLFW_PRESS) {
+		cameraX = cameraX + 0.2f;
+	}
+	if (key == GLFW_KEY_LEFT && action == GLFW_PRESS) {
+		cameraX = cameraX - 0.2f;
+	}
 }
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
 	projection_matrix = glm::perspective(45.0f, (GLfloat)width / (GLfloat)height, 0.0f, 100.0f);
